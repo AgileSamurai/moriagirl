1e0b41aca5301fb8423aa4a9c5906b7126d1a918
Manabu Ito <m12111002@yahoo.co.jp>
2016-08-26 18:09:28 +0900

    to read text file

---
+++ app/src/main/java/com/sample/agilesamurai/moriagirl/ReadTextFile.java
@@ -0,0 +1,41 @@
+package com.sample.agilesamurai.moriagirl;
+
+import android.content.Context;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.io.BufferedReader;
+
+
+/**
+ * Created by ito on 2016/08/26.
+ */
+public class ReadTextFile {
+    InputStream is = null;
+    BufferedReader br = null;
+    String text = "";
+
+    public String TextContents(){
+        return text;
+    }
+
+    public ReadTextFile(Context context) {
+        try {
+            try {
+                // assetsフォルダ内の sample.txt をオープンする
+                is = context.getAssets().open("sample.txt");
+                br = new BufferedReader(new InputStreamReader(is));
+
+                // １行ずつ読み込み、改行を付加する
+                String str;
+                while ((str = br.readLine()) != null) {
+                    text += str + "\n";
+                }
+            } finally {
+                if (is != null) is.close();
+                if (br != null) br.close();
+            }
+        } catch (Exception e) {
+            // エラー発生時の処理
+        }
+    }
+}
