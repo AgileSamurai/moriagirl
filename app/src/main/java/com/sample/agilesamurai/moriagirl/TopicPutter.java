35e8b443ed44a033f525c3618e7d7913fdba13e3
Manabu Ito <m12111002@yahoo.co.jp>
2016-08-29 11:26:29 +0900

    add random prototype

--- app/src/main/java/com/sample/agilesamurai/moriagirl/TopicPutter.java
+++ app/src/main/java/com/sample/agilesamurai/moriagirl/TopicPutter.java
@@ -4,21 +4,28 @@ import android.content.Context;
 import java.io.InputStreamReader;
 import java.io.InputStream;
 import java.io.BufferedReader;
+import java.util.Random;
 
 
 /**
  * Created by ito on 2016/08/26.
  */
 public class TopicPutter {
-    InputStream is = null;
-    BufferedReader br = null;
-    String text = "";
+    InputStream is;
+    BufferedReader br;
+    String text;
+
+    TopicPutter() {
+        is = null;
+        br = null;
+        text = "";
+    }
 
     public String TextContents(){
         return text;
     }
 
-    public TopicPutter(Context context) {
+    public void TextPutter(Context context){
         try {
             try {
                 // assetsフォルダ内の sample.txt をオープンする
@@ -38,4 +45,33 @@ public class TopicPutter {
             // エラー発生時の処理
         }
     }
+
+    public void RandomTextPutter(Context context){
+        try {
+            try {
+                // assetsフォルダ内の sample.txt をオープンする
+                is = context.getAssets().open("topic.txt");
+                br = new BufferedReader(new InputStreamReader(is));
+
+                //1から10までrandom
+                Random rnd = new Random();
+                int ran = rnd.nextInt(11) + 1;
+
+                // １行ずつ読み込み、改行を付加する
+                String str;
+                int count = 0;
+                while ((str = br.readLine()) != null) {
+                    count++;
+                    if(count == ran)
+                    text += str + "\n";
+                }
+            } finally {
+                if (is != null) is.close();
+                if (br != null) br.close();
+            }
+        } catch (Exception e) {
+            // エラー発生時の処理
+        }
+    }
+
 }
