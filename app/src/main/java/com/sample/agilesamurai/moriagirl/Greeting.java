c5f9da580faad44ace57d647f0dbeda0829e037c
shihakata <shihakata3456@gmail.com>
2016-08-29 19:42:41 +0900

    change greeting

--- app/src/main/java/com/sample/agilesamurai/moriagirl/Greeting.java
+++ app/src/main/java/com/sample/agilesamurai/moriagirl/Greeting.java
@@ -8,17 +8,22 @@ import android.content.Context;
 public class Greeting {
     Talking talking;
     String gr1;
+    TextRead inputfile;
 
     public Greeting(Context context_input){
-        gr1 = "こんにちは、もりあガールです";
+       // gr1 = "こんにちは、もりあガールです";
         talking = new Talking(context_input);
+        inputfile = new TextRead(context_input,"greeting.txt");
     }
 
     public void startGreeting(){
+        gr1 = inputfile.simpleTextRead(0);
         talking.talk(gr1);
     }
 
+
     public void randomGreeting(){
-        //TopicPutterのrandomTextPutのtopic.txtを変えればすぐできるが・・・
+        gr1 = inputfile.randomTextRead();
+        talking.talk(gr1);
     }
 }
