bfd5224bd546b323de43a106810ba99040b0a166
shihakata <shihakata3456@gmail.com>
2016-08-29 20:26:37 +0900

    change_topicputter

--- app/src/main/java/com/sample/agilesamurai/moriagirl/TopicPutter.java
+++ app/src/main/java/com/sample/agilesamurai/moriagirl/TopicPutter.java
@@ -12,18 +12,16 @@ import java.util.Random;
  * Created by ito on 2016/08/26.
  */
 public class TopicPutter {
-    InputStream inputstream;
-    BufferedReader bufferedreader;
     String text;
     Activity activity;
     Talking talking;
+    TextRead inputfile;
 
     TopicPutter(Activity context_input) {
-        inputstream = null;
-        bufferedreader= null;
         text = "";
         activity = context_input;
         talking = new Talking(activity);
+        inputfile = new TextRead(context_input,"topic.txt");
     }
 
     public String textContents(){
@@ -33,62 +31,14 @@ public class TopicPutter {
 
     //とりあえず１行目のお題表示
     public void textPut(){
-        try {
-            try {
-                // assetsフォルダ内の sample.txt をオープンする
-                inputstream = activity.getAssets().open("sample.txt");
-                bufferedreader = new BufferedReader(new InputStreamReader(inputstream));
-
-                // １行ずつ読み込み、改行を付加する
-                text = bufferedreader.readLine();
-
-                //話す
-                talking.talk(text);
-
-            } finally {
-                //ファイルを閉じる
-                if (inputstream != null) inputstream.close();
-                if (bufferedreader != null) bufferedreader.close();
-            }
-        } catch (Exception e) {
-            // エラー発生時の処理
-        }
+        text = inputfile.simpleTextRead(0);
+        talking.talk(text);
     }
 
     //10行の中でランダムで１行表示
     public void randomTextPut(){
-        try {
-            try {
-                // assetsフォルダ内のtopic.txt をオープンする
-                inputstream = activity.getAssets().open("topic.txt");
-                bufferedreader = new BufferedReader(new InputStreamReader(inputstream));
-
-                //1から10までrandom
-                Random rnd = new Random();
-                int ran = rnd.nextInt(10) + 1;
-
-                // １行ずつ読み込む
-                String str;
-                int count = 0;
-                while ((str = bufferedreader.readLine()) != null) {
-                    count++;
-                    if(count == ran) {
-                        text = str;
-                        break;
-                    }
-                }
-
-                //話す
-                talking.talk(text);
-
-            } finally {
-                //ファイルを閉じる
-                if (inputstream != null) inputstream.close();
-                if (bufferedreader != null) bufferedreader.close();
-            }
-        } catch (Exception e) {
-            // エラー発生時の処理
-        }
+        text = inputfile.randomTextRead();
+        talking.talk(text);
     }
 
 }
