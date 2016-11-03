c8275bf18a0e55b781598fb2561db621ca8c1b94
motoki nagaoka <motoki.23ar@gmail.com>
2016-11-03 13:27:18 +0900

    AudioRecordでやってみたけど値が入ってこない

--- app/src/main/java/com/sample/agilesamurai/moriagirl/MainActivity.java
+++ app/src/main/java/com/sample/agilesamurai/moriagirl/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -49,6 +50,12 @@ public class MainActivity extends AppCompatActivity {
         topicPutter = new TopicPutter(this);
         byebye = new Byebye(this);
         greeting.randomGreeting();
+
+        MoriagariSensor moriagariSensor = new MoriagariSensor();
+        Thread moriagariSensorThread = new Thread(moriagariSensor);
+        moriagariSensorThread.run();
+        Log.d("MainActivity", "onCreate:" );
+
     }
 
     @Override
