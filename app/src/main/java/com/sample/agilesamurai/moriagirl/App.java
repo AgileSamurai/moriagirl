ec46b0826f6ed02427fc683df2939b9b8377e596
Ibara <ibara1454@gmail.com>
2016-11-13 14:13:44 +0900

    Added SoundMeterActivity and XML design, and implemented App

--- app/src/main/java/com/sample/agilesamurai/moriagirl/App.java
+++ app/src/main/java/com/sample/agilesamurai/moriagirl/App.java
@@ -2,9 +2,45 @@ package com.sample.agilesamurai.moriagirl;
 
 import android.app.Application;
 
+import com.sample.agilesamurai.moriagirl.models.SoundMeterDataModel;
+import com.sample.agilesamurai.moriagirl.models.SoundMeterModel;
+
+import rx.schedulers.Schedulers;
+import rx.subscriptions.CompositeSubscription;
+
 /**
  * Created by ibara on 2016/11/13.
  */
 
 public class App extends Application {
+    private SoundMeterModel soundMeter;
+    private SoundMeterDataModel soundMeterData;
+
+    CompositeSubscription subscriptions = new CompositeSubscription();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        soundMeter = new SoundMeterModel();
+        soundMeterData = new SoundMeterDataModel();
+        subscriptions.add(soundMeter.getOutStream()
+                .subscribeOn(Schedulers.newThread()) // Schedulers.io() uses thread pool, whereas Schedulers.newThread() does not
+                .observeOn(Schedulers.newThread())
+                .subscribe(soundMeterData.getInStream()));
+        soundMeter.start();
+    }
+
+    @Override
+    public void onTerminate() {
+        subscriptions.unsubscribe();
+        super.onTerminate();
+    }
+
+    public SoundMeterModel getSoundMeterModel() {
+        return soundMeter;
+    }
+
+    public SoundMeterDataModel getSoundMeterDataModel() {
+        return soundMeterData;
+    }
 }
